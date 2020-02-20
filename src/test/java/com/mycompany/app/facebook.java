7a87b426e06333f26b219d958c1c9097980ee43c
karuppiah <karuppiahkannan92@gmail.com>
2020-02-20 21:35:00 +0000

    added import file for desired capabilities

--- src/test/java/com/mycompany/app/facebook.java
+++ src/test/java/com/mycompany/app/facebook.java
@@ -8,6 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class facebook {
 	 WebDriver driver;
--- src/test/java/com/mycompany/app/google.java
+++ src/test/java/com/mycompany/app/google.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.MalformedURLException;
 
 public class google {
